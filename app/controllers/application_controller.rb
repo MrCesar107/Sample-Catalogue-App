b0e7e4c2d3a12b82f0ea2ff1893d47a5136a80c1
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2020-12-03 22:52:20 -0600

    Devise views redisign

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
-class ApplicationController < ActionController::Base
+# frozen_string_literal: true
+
+class ApplicationController < ActionController::Base # :nodoc:
+  before_action :configure_permitted_params, if: :devise_controller?
+  protect_from_forgery with: :exception
+  force_ssl if: :ssl_configured?
+
+  def ssl_configured?
+    !Rails.env.development?
+  end
+
+  protected
+
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
